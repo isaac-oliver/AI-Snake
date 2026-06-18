fbf008a76f7faf6009a8692b0dbc20a359207c08
Isaac Oliver <isaac051307@gmail.com>
2026-06-18 11:37:02 +0800

    Added Snake

--- snake.py
+++ snake.py
@@ -6,6 +6,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Snake Game")
 FPS = 10
 FramePerSec = pygame.time.Clock()
+snake_pos = [100, 300]
 running = True
 while running:
     for event in pygame.event.get():
@@ -13,7 +14,7 @@ while running:
             running = False
 
     screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(snake_pos[0], snake_pos[1], 20, 20))
     pygame.display.update()
     FramePerSec.tick(FPS)
 pygame.quit()
-
