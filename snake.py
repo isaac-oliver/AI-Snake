7e324865f36fc21b3b50525ad94ce172ed94275f
Isaac Oliver <isaac051307@gmail.com>
2026-06-18 12:10:11 +0800

    Slowed Movement Speed

--- snake.py
+++ snake.py
@@ -1,15 +1,17 @@
 import pygame # type: ignore
 pygame.init()
 
-WIDTH, HEIGHT = 600, 600
+WIDTH, HEIGHT = 400, 400
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Snake Game")
-FPS = 10
+FPS = 60
 direction = "RIGHT"
 FramePerSec = pygame.time.Clock()
 snake_pos = [100, 300]
 running = True
-
+move_del = 250
+lastmove = 0
+current_time = 0
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -18,6 +20,7 @@ while running:
     screen.fill((0, 0, 0))
     pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(snake_pos[0], snake_pos[1], 20, 20))
     key = pygame.key.get_pressed()
+    current_time = pygame.time.get_ticks()
     if key[pygame.K_RIGHT]:
         direction = "RIGHT"
     elif key[pygame.K_LEFT]:
@@ -26,14 +29,18 @@ while running:
         direction = "UP"
     elif key[pygame.K_DOWN]:
         direction = "DOWN"
-    if direction == "RIGHT":
-        snake_pos[0] += 20
-    elif direction == "LEFT":
-        snake_pos[0] -= 20
-    elif direction == "UP":
-        snake_pos[1] -= 20
-    elif direction == "DOWN":
-        snake_pos[1] += 20
+        
+    if current_time - lastmove > move_del:
+
+        if direction == "RIGHT":
+            snake_pos[0] += 20
+        elif direction == "LEFT":
+            snake_pos[0] -= 20
+        elif direction == "UP":
+            snake_pos[1] -= 20
+        elif direction == "DOWN":
+            snake_pos[1] += 20
+        lastmove = current_time
     
     pygame.display.update()
     FramePerSec.tick(FPS)
