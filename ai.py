5da43367492942faa82b204d1c9419821f3542f7
Isaac Oliver <isaac051307@gmail.com>
2026-06-23 09:41:48 +0800

    Added Smarter AI

--- ai.py
+++ ai.py
@@ -1,10 +1,34 @@
-def choose_direction(snake_head,apple_pos,width,height):
+def choose_direction(snake_head,apple_pos,direction,width,height):
     if snake_head[0] < apple_pos[0]:
-        return "RIGHT"
+        if direction == "LEFT":
+            if snake_head[1] > height - 20:
+                return "DOWN"
+            else:
+                return "UP"
+        else:   
+            return "RIGHT"
     elif snake_head[0] > apple_pos[0]:
-        return "LEFT"
+        if direction == "RIGHT":
+            if snake_head[1] > height - 20:
+                return "DOWN"
+            else:
+                return "UP"
+        else:
+            return "LEFT"
     elif snake_head[1] < apple_pos[1]:
-        return "DOWN"
+        if direction == "UP":
+            if snake_head[0] > width - 20:
+                return "RIGHT"
+            else:
+                return "LEFT"
+        else:   
+            return "DOWN"
     elif snake_head[1] > apple_pos[1]:
-        return "UP"
+        if direction == "DOWN":
+            if snake_head[0] > width - 20:
+                return "RIGHT"
+            else:
+                return "LEFT"
+        else:   
+            return "UP"
     
