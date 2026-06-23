11704fd7b76f6fb06348dd10f6cbd9e32144e45d
Isaac Oliver <isaac051307@gmail.com>
2026-06-23 11:13:46 +0800

    Added Collision Checking (first attempt)

--- ai.py
+++ ai.py
@@ -1,34 +1,78 @@
-def choose_direction(snake_head,apple_pos,direction,width,height):
-    if snake_head[0] < apple_pos[0]:
-        if direction == "LEFT":
-            if snake_head[1] > height - 20:
-                return "DOWN"
+def choose_direction(snake_pos,snake_head,apple_pos,direction,width,height):
+    if check_collision(snake_pos,snake_head,direction) != False:
+        return check_collision(snake_pos,snake_head,direction)
+    else:
+        if snake_head[0] < apple_pos[0]:
+            if direction == "LEFT":
+                if snake_head[1] > height - 20:
+                    return "DOWN"
+                else:
+                    return "UP"
             else:
-                return "UP"
-        else:   
-            return "RIGHT"
-    elif snake_head[0] > apple_pos[0]:
-        if direction == "RIGHT":
-            if snake_head[1] > height - 20:
-                return "DOWN"
-            else:
-                return "UP"
-        else:
-            return "LEFT"
-    elif snake_head[1] < apple_pos[1]:
-        if direction == "UP":
-            if snake_head[0] > width - 20:
                 return "RIGHT"
+        elif snake_head[0] > apple_pos[0]:
+            if direction == "RIGHT":
+                if snake_head[1] > height - 20:
+                    return "DOWN"
+                else:
+                    return "UP"
             else:
                 return "LEFT"
-        else:   
-            return "DOWN"
-    elif snake_head[1] > apple_pos[1]:
-        if direction == "DOWN":
-            if snake_head[0] > width - 20:
-                return "RIGHT"
+        elif snake_head[1] < apple_pos[1]:
+            if direction == "UP":
+                if snake_head[0] > width - 20:
+                    return "RIGHT"
+                else:
+                    return "LEFT"
             else:
-                return "LEFT"
-        else:   
-            return "UP"
+                return "DOWN"
+        elif snake_head[1] > apple_pos[1]:
+            if direction == "DOWN":
+                if snake_head[0] > width - 20:
+                    return "RIGHT"
+                else:
+                    return "LEFT"
+            else:
+                return "UP"
+
+
+def check_collision(snake_pos,snake_head,direction):
+    if direction == "RIGHT":
+        for block in snake_pos[1:]:
+            if snake_head[0] + 20 == block[0] and snake_head[1] == block[1]:
+                if snake_head[1] + 20 == block[1]:
+                    return "DOWN"
+                else:
+                    return "UP"
+            else:
+                return False    
+    elif direction == "LEFT":
+        for block in snake_pos[1:]:
+            if snake_head[0] - 20 == block[0] and snake_head[1] == block[1]:
+                if snake_head[1] + 20 == block[1]:
+                    return "DOWN"
+                else:
+                    return "UP"
+            else:
+                return False
+    elif direction == "UP":
+        for block in snake_pos[1:]:
+            if snake_head[0] == block[0] and snake_head[1] - 20 == block[1]:
+                if snake_head[0] + 20 == block[0]:
+                    return "RIGHT"
+                else:
+                    return "LEFT"
+            else:
+                return False
+    elif direction == "DOWN":
+        for block in snake_pos[1:]:
+            if snake_head[0] == block[0] and snake_head[1] + 20 == block[1]:
+                if snake_head[0] + 20 == block[0]:
+                    return "RIGHT"
+                else:
+                    return "LEFT"
+            else:
+                return False
+
+    
     
